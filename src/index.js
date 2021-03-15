ef6fb128759fb1e2fd0ed9c3b2bad03cf0143f62
rich11berr <rinocraft.tv@gmail.com>
2021-03-16 01:26:46 +0400

    feat: 2nd try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(typeof str !== "string") {
+    return false;
+  }
+  let lengthB = bracketsConfig.length;
+  let brackets = "";
+  let different = "";
+  let count = 0;
+
+  while (count !== lengthB) {
+    count=0;
+    for (let i = 0; i > lengthB; i++) {
+      brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+      different = str.replace(brackets, "");
+      if (str === different) {
+        count++;
+      }
+      str = different;
+    }
+  }
+  if(str.length === 0) {
+    return true;
+  } return false;
 }
